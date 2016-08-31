d1d6399bca21637e1816789148ab67dc86f993e1
Kevin Huelsmann <kjh311@gmail.com>
2016-08-31 08:49:32 -0700

    first

--- gulpfile.js
+++ gulpfile.js
@@ -15,11 +15,11 @@ gulp.task('hello', function(){
 //     .pipe(gulp.dest('app/scss/styles.css'))
 // });
 
-gulp.task('sass', function(){
-    sass('app/scss/styles.scss')
-        .on('error', sass.logError)
-        .pipe(gulp.dest('app/scss/styles2.css'))
-});
+// gulp.task('sass', function(){
+//     sass('app/scss/styles.scss')
+//         .on('error', sass.logError)
+//         .pipe(gulp.dest('app/scss/styles2.css'))
+// });
 
 gulp.task('browserSync', function() {
   browserSync.init({
@@ -40,4 +40,23 @@ gulp.task('blah', function() {
     (browserSync.reload({
       stream: true
     }))
+});
+
+
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./app"
+    });
+
+    gulp.watch('app/scss/styles.scss', ['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    sass('app/scss/styles.scss')
+        .on('error', sass.logError)
+        .pipe(gulp.dest('app/scss/styles2.css'))
+        .pipe(browserSync.stream());
 });
